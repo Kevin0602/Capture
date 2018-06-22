83f3da9429bb874b48983e006244592b0e5880ce
Kevin <zhangzmkevin@gmail.com>
2018-06-22 18:38:50 +0800

    GPU+MulThread

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 //Opencv includes
 #include <opencv2/core/utility.hpp>
 #include <opencv2/opencv.hpp>
@@ -7,9 +6,9 @@
 #include "opencv2/videoio.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/calib3d.hpp"
-//#include "cuda.h"
 #include "opencv2/core/cuda.hpp"
-//#include "opencv2/gpu/gpu.hpp"
+#include "opencv2/cudaimgproc.hpp"
+
 
 //Stander includes
 #include <math.h>
@@ -17,6 +16,8 @@
 #include <ctype.h>
 #include <thread>
 #include <queue>
+#include <vector>
+
 // ZED includes
 #include <sl/Camera.hpp>
 
@@ -28,33 +29,84 @@ using namespace sl;
 using namespace cv;
 using namespace std;
 
+
+
+//Main data struct
+typedef struct {
+
+	cv::cuda::GpuMat L;
+	cv::cuda::GpuMat R;
+	bool finish;
+	cv::Mat position;
+}IMAGE_PAIR;
+
+vector<IMAGE_PAIR> image_pair_vector;
+
+//Varibales for mouse selected and trackbar
+bool backprojMode = false;
+bool selectObject = false;
+int trackObject = 0;
+bool showHist = true;
+Point origin;
+Rect selection;
+int vmin = 10, vmax = 256, smin = 30;
+
+//Image on cpu for showing and drawing result on
+cv::Mat image_ocv_L, image_ocv_R;
+
+//Histogram of the object selected
+cv::Mat hist;
+
+//Mutex of all threads
+Mutex mtx;
+
+// Create a ZED camera object
+Camera zed;
+
 // draw a box around object to track. This triggers CAMShift to start tracking
-static void onMouse( int event, int x, int y, int, void* )
+static void onMouse(int event, int x, int y, int, void*)
 {
-    if( selectObject )
-    {
-        selection.x = MIN(x, origin.x);
-        selection.y = MIN(y, origin.y);
-        selection.width = std::abs(x - origin.x);
-        selection.height = std::abs(y - origin.y);
-        selection &= Rect(0, 0, image_ocv.cols, image_ocv.rows);
-    }
-    switch( event )
-    {
-    case EVENT_LBUTTONDOWN:
-        origin = Point(x,y);
-        selection = Rect(x,y,0,0);
-        selectObject = true;
-        break;
-    case EVENT_LBUTTONUP:
-        selectObject = false;
-        if( selection.width > 0 && selection.height > 0 )
-            trackObject = -1;   // Set up CAMShift properties in main() loop
-        break;
-    }
+	if (selectObject)
+	{
+		selection.x = MIN(x, origin.x);
+		selection.y = MIN(y, origin.y);
+		selection.width = std::abs(x - origin.x);
+		selection.height = std::abs(y - origin.y);
+		selection &= Rect(0, 0, image_ocv_L.cols, image_ocv_L.rows);
+	}
+	switch (event)
+	{
+	case EVENT_LBUTTONDOWN:
+		origin = Point(x, y);
+		selection = Rect(x, y, 0, 0);
+		selectObject = true;
+		break;
+	case EVENT_LBUTTONUP:
+		selectObject = false;
+		if (selection.width > 0 && selection.height > 0)
+			trackObject = -1;   // Set up CAMShift properties in main() loop
+		break;
+	}
 }
 
 //Covert sl mat to cv mat gpu
+sl::Mat cvGpuMat2slGpuMat(cv::cuda::GpuMat& input)
+{
+	// Mapping between MAT_TYPE and CV_TYPE
+	sl::MAT_TYPE type;
+	switch (input.type()) {
+
+	case  CV_8UC3: type = MAT_TYPE_8U_C3; break;
+	case  CV_8UC4: type = MAT_TYPE_8U_C4; break;
+	default: break;
+	}
+
+	// Since cv::Mat data requires a uchar* pointer, we get the uchar1 pointer from sl::Mat (getPtr<T>())
+	// cv::Mat and sl::Mat will share a single memory structure
+	return sl::Mat(size_t(input.rows), size_t(input.cols), type, (sl::uchar1*)input.data, input.step, MEM_GPU);
+}
+
+
 cv::cuda::GpuMat slGpuMat2cvGpuMat(sl::Mat& input)
 {
 	// Mapping between MAT_TYPE and CV_TYPE
@@ -73,21 +125,29 @@ cv::cuda::GpuMat slGpuMat2cvGpuMat(sl::Mat& input)
 
 	// Since cv::Mat data requires a uchar* pointer, we get the uchar1 pointer from sl::Mat (getPtr<T>())
 	// cv::Mat and sl::Mat will share a single memory structure
-	return cv::cuda::GpuMat(input.getHeight(), input.getWidth(), cv_type, input.getPtr<sl::uchar1>(MEM_GPU),input.getStepBytes(MEM_GPU));
+	return cv::cuda::GpuMat(input.getHeight(), input.getWidth(), cv_type, input.getPtr<sl::uchar1>(MEM_GPU), input.getStepBytes(MEM_GPU));
 }
 
-typedef struct{
 
-  cv::cuda::GpuMat L;
-  cv::cuda::GpuMat R;
 
-}IMAGE_PAIR
+void camshift_thread(cuda::GpuMat& img)
+{
+	cuda::GpuMat hsv;
+	cuda::cvt
+
+}
+
+
+void process_thread()
+{
+
+
+}
+
 
 
 int main()
 {
-	// Create a ZED camera object
-	Camera zed;
 
 	// Set configuration parameters
 	InitParameters init_params;
@@ -107,31 +167,45 @@ int main()
 
 	// Set runtime parameters after opening the camera
 
-    //zed.setCameraSettings(CAMERA_SETTINGS_SATURATION,8);
-    // zed.setCameraSettings(CAMERA_SETTINGS_CONTRAST,4);
-    //zed.setCameraSettings(CAMERA_SETTINGS_GAIN,60);
-    //zed.setCameraSettings(CAMERA_SETTINGS_EXPOSURE,50);
+	//zed.setCameraSettings(CAMERA_SETTINGS_SATURATION,8);
+	//zed.setCameraSettings(CAMERA_SETTINGS_CONTRAST,4);
+	//zed.setCameraSettings(CAMERA_SETTINGS_GAIN,60);
+	//zed.setCameraSettings(CAMERA_SETTINGS_EXPOSURE,50);
 	RuntimeParameters runtime_parameters;
 	runtime_parameters.sensing_mode = SENSING_MODE_STANDARD;
 
 
 	//Define some variables to be use later and allocate memory
 	Resolution image_size = zed.getResolution();
-	sl::Mat image_zed_L(image_size, MAT_TYPE_8U_C4,MEM_GPU);
-	cv::cuda::GpuMat image_cvGPU_L = slGpuMat2cvGpuMat(image_zed);
-	cv::Mat image_cvCPU_L(image_size.height, image_size.width,image_cvGPU.type());
+	cuda::HostMem MEM_L(image_size.height, image_size.width, CV_8UC3, cuda::HostMem::SHARED);
+	cv::cuda::GpuMat image_cvGPU_L = MEM_L.createGpuMatHeader();
+	sl::Mat image_zed_L = cvGpuMat2slGpuMat(image_cvGPU_L);
+	cv::Mat image_cvCPU_L = MEM_L.createMatHeader();
 
-	sl::Mat image_zed_R(image_size, MAT_TYPE_8U_C4,MEM_GPU);
-	cv::cuda::GpuMat image_cvGPU_R = slGpuMat2cvGpuMat(image_zed);
-	cv::Mat image_cvCPU_R(image_size.height, image_size.width,image_cvGPU.type());
+	cuda::HostMem MEM_R(image_size.height, image_size.width, CV_8UC3, cuda::HostMem::SHARED);
+	cv::cuda::GpuMat image_cvGPU_R = MEM_R.createGpuMatHeader();
+	sl::Mat image_zed_R = cvGpuMat2slGpuMat(image_cvGPU_R);
+	cv::Mat image_cvCPU_R = MEM_L.createMatHeader();
 
-    cv::cuda::Stream stream;
-    IMAGE_PAIR image_temp;
-    queue<IMAGE_PAIR> image_queue;
+	cv::cuda::Stream stream;
+	IMAGE_PAIR image_temp;
 
+
+
+	namedWindow("L", 0);
+	setMouseCallback("L", onMouse, 0);
+	createTrackbar("Vmin", "L", &vmin, 256, 0);
+	createTrackbar("Vmax", "L", &vmax, 256, 0);
+	createTrackbar("Smin", "L", &smin, 256, 0);
+
+	int hsize = 4;
+	float hranges[] = { 0,180 };
+	const float* phranges = hranges;
 
 	bool stop = false;
 	char c;
+
+	vector<int> a;
 
 	while (!stop)
 	{
@@ -141,10 +215,14 @@ int main()
 
 			// Retrieve the left image, depth image in half-resolution
 			zed.retrieveImage(image_zed_L, VIEW_LEFT, MEM_GPU);
-            zed.retrieveImage(image_zed_R, VIEW_RIGHT, MEM_GPU);
-            image_temp.L = image_cvG
-
-
+			zed.retrieveImage(image_zed_R, VIEW_RIGHT, MEM_GPU);
+			image_temp.L = image_cvGPU_L;
+			image_temp.R = image_cvGPU_R;
+			image_temp.finish = false;
+			image_temp.position = cv::Mat::zeros(4, 1, CV_32FC1);
+			while (!mtx.trylock()) {}
+			image_pair_vector.push_back(image_temp);
+			mtx.unlock();
 		}
 		else
 		{
@@ -152,22 +230,55 @@ int main()
 			break;
 		}
 
-
-        image_cvCPU = cv::Mat(image_size.height, image_size.width,image_cvGPU.type());
-		//cv::cuda::GpuMat Image_ocv_gpu = image_cvGPU.clone();
-		image_cvGPU.download(image_cvCPU,stream);
-
-		imshow("123", image_cvCPU);
-
-		c = waitKey(0);
-		if (c == 'q') stop = true;
+		if (trackObject)
+		{
 
 
+			if (trackObject < 0)
+			{
+				// Object has been selected by user, set up CAMShift search properties once
+				cv::Mat hsv, mask;
+				cvtColor(image_cvCPU_L, hsv, COLOR_BGR2HSV);
+				int _vmin = vmin, _vmax = vmax;
+				inRange(hsv, Scalar(0, smin, MIN(_vmin, _vmax)),
+					Scalar(180, 256, MAX(_vmin, _vmax)), mask);
+				int ch[] = { 0, 0 };
+				cv::Mat hue;
+				hue.create(hsv.size(), hsv.depth());
+				mixChannels(&hsv, 1, &hue, 1, ch, 1);
+
+				cv::Mat roi(hue, selection), maskroi(mask, selection);
+				calcHist(&roi, 1, 0, maskroi, hist, 1, &hsize, &phranges);
+				normalize(hist, hist, 0, 255, NORM_MINMAX);
+				trackObject = 1; // Don't set up again, unless user selects new ROI
+			}
+
+		}
 
 
-
+		imshow("L", image_cvCPU_L);
+		char c = (char)waitKey(1);
+		if (c == 27)
+			break;
+		switch (c)
+		{
+		case 'b':
+			backprojMode = !backprojMode;
+			break;
+		case 'h':
+			showHist = !showHist;
+			if (!showHist)
+				destroyWindow("Histogram");
+			else
+				namedWindow("Histogram", 1);
+			break;
+		default:
+			;
+		}
 
 
 	}
-    zed.close();
+	zed.close();
 }
+
+
